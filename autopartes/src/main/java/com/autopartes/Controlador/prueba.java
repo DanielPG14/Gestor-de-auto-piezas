a761202f2c6c86265fc5b64ce2bcfcfac507f633
Daniel Pineda <daniel.pineda.gomez14@gmail.com>
2026-05-08 09:05:44 -0600

    Singleton

---
+++ autopartes/src/main/java/com/autopartes/Controlador/prueba.java
@@ -0,0 +1,24 @@
+package com.autopiezas.controlador;
+
+import com.autopiezas.modelo.Conexion;
+import javafx.fxml.FXML;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class prueba {
+
+    @FXML
+    private void manejarBotonPrueba() {
+        System.out.println("LOG: Intentando conectar a DBA...");
+        
+        try (Connection db = Conexion.getInstancia()) {
+            if (db != null && !db.isClosed()) {
+                System.out.println("¡ÉXITO!: La conexión a DBA está activa y lista.");
+            } else {
+                System.out.println("ERROR: La conexión regresó nula.");
+            }
+        } catch (SQLException e) {
+            System.err.println("ERROR de SQL: " + e.getMessage());
+        }
+    }
+}
