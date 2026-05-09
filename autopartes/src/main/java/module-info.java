217f51da20ee75604bc4051ffe938be3fa8218a3
DanielPG <daniel.pineda.gomez14@gmail.com>
2026-05-09 13:34:50 -0600

    Corrección

--- autopartes/src/main/java/module-info.java
+++ autopartes/src/main/java/module-info.java
@@ -1,4 +1,13 @@
-module com.autopartes {
+module autopartes {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    // Intentamos con el nombre de módulo automático estándar
+    requires mysql.connector.j; 
+
+    // Abrimos los paquetes para que Scene Builder y JavaFX funcionen
+    opens com.autopartes to javafx.fxml;
+    opens com.autopartes.Controlador to javafx.fxml;
+    
     exports com.autopartes;
 }
