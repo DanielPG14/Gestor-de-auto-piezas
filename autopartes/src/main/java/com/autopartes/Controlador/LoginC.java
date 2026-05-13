3f63f83fae7a91b7786231bfa975ba69dc00dbcf
DanielPG <daniel.pineda.gomez14@gmail.com>
2026-05-12 19:19:47 -0600

    Conexiones

---
+++ autopartes/src/main/java/com/autopartes/Controlador/LoginC.java
@@ -0,0 +1,52 @@
+package com.autopartes.Controlador;
+
+import java.io.IOException;
+
+import com.autopartes.Modelo.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+public class LoginC {
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private PasswordField txtPass;
+    @FXML
+    private Label lblError;
+
+    private UsuarioDAO dao = new UsuarioDAO();
+
+    @FXML
+    private void manejarLogin(ActionEvent event) {
+        String user = txtUser.getText();
+        String pass = txtPass.getText();
+
+        Usuario u = dao.login(user, pass);
+        if (u != null) {
+            Sesion.setUsuario(u);
+            System.out.println("VINCULACIÓN EXITOSA: " + Sesion.getUsuario().getUsername());
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/com/autopartes/vistas/CatalogoVendedor.fxml"));
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setTitle("Panel Principal - " + u.getRol()); // Título dinámico
+                stage.show();
+
+            } catch (IOException e) {
+                lblError.setText("Error al cargar la ventana principal.");
+                e.printStackTrace();
+            }
+        } else {
+            lblError.setText("Error: Usuario no encontrado.");
+        }
+    }
+}
--- autopartes/src/main/java/com/autopartes/Modelo/UsuarioDAO.java
+++ autopartes/src/main/java/com/autopartes/Modelo/UsuarioDAO.java
@@ -1,15 +1,10 @@
 package com.autopartes.Modelo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UsuarioDAO {
     public Usuario login(String user, String pass) {
-        // Usamos 'users' que es la tabla que no tiene conflictos de archivos
         String sql = "SELECT IDusuario, username, rol FROM users WHERE username = ? AND password = ?";
-        
         try (Connection db = Conexion.getInstancia();
              PreparedStatement ps = db.prepareStatement(sql)) {
             
@@ -25,7 +20,7 @@ public class UsuarioDAO {
                 );
             }
         } catch (SQLException e) {
-            System.err.println("Error en el login: " + e.getMessage());
+            e.printStackTrace();
         }
         return null;
     }
